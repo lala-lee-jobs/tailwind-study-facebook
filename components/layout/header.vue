b8be6f7d3cd8d7e39f724261940d4a702a632606
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-02-28 19:33:04 +0800

    feat: 實戰仿Facebook UI，Navbar 實作

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,5 +1,35 @@
 <template>
-  <header></header>
+  <header class="flex items-center justify-between px-4">
+    <!-- header left -->
+    <div class="flex items-center">
+      <!-- FB logo -->
+      <svg-icon name="logo" class="h-[39px] w-[39px] bg-fb rounded-full"/>
+      <!-- Search Icon (只在小版顯示)-->
+      <button class="popover-btn lg:hidden">
+        <svg-icon name="search"/>
+      </button>
+      <!-- Search Bar (只在大版顯示)-->
+      <div class="bg-fb-input ml-2 py-2 px-3 w-[240px] rounded-full flex-shrink items-center hidden lg:flex">
+        <svg-icon name="search" class="w-[18px] h-[18px] mr-2"/>
+        <input type="text" class="w-full bg-transparent text-white focus:outline-none" placeholder="搜尋 Facebook">
+      </div>
+    </div>
+    <!-- header right -->
+    <div class="flex items-center">
+      <button class="popover-btn">
+        <svg-icon name="plus"/>
+      </button>
+      <button class="popover-btn">
+        <svg-icon name="messenger"/>
+      </button>
+      <button class="popover-btn">
+        <svg-icon name="bell"/>
+      </button>
+      <button class="popover-btn">
+        <svg-icon name="down"/>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -37,20 +37,24 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    // https://github.com/nuxt-community/svg-sprite-module
+    '@nuxtjs/svg-sprite',
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
-
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  svgSprite: {
+    // manipulate module options
+    input: '~/assets/icons/svg',
+    output:'~/assets/icons/sprite'
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
 }
