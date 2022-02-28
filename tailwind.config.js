8904cda5c3564c008d2ec2f8ac7202089463fe27
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-02-28 14:18:59 +0800

    feat: 實戰仿Facebook UI Tailwind 主題config配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#3e4042',
+        'fb-active': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff'
+      }
     },
   },
   variants: {
