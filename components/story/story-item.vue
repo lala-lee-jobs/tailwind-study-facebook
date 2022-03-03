9b8bf869bf9d386ac452c2b24506f7136a195c89
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-03-03 23:13:31 +0800

    feat: 實戰仿Facebook UI，限時動態4 整理重覆的部分(抽成story-item)

---
+++ components/story/story-item.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    :key="id"
+    class="min-w-[120px] flex-1 cursor-pointer px-[4px]"
+    @mouseover="hoverId = id"
+    @mouseleave="hoverId = ''"
+  >
+    <div class="relative overflow-hidden">
+      <div
+        :class="{ hidden: hoverId === id }"
+        class="absolute top-0 left-0 h-full w-full bg-black/20"
+      ></div>
+      <!-- 限動帳號頭像 -->
+      <div
+        class="absolute top-4 left-4 z-10 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-fb-card ring-4 ring-fb"
+      >
+        <p class="text-sm text-white">布</p>
+      </div>
+      <!-- 漸層的遮罩 -->
+      <div
+        class="absolute top-0 left-0 z-20 h-full w-full bg-gradient-to-b from-black/30 to-transparent"
+      ></div>
+      <img
+        :class="{ 'scale-105': hoverId === id }"
+        class="h-full w-full duration-500 hover:scale-105"
+        src="https://bruce-fe-fb.web.app/image/story.png"
+        alt=""
+      />
+      <!-- 限動帳號名稱 -->
+      <div class="absolute bottom-2 left-2 z-10 text-white">布魯斯</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'StoryItem',
+    props: {
+      id: {
+        type: String,
+        default: '',
+      },
+    },
+    data() {
+      return {
+        hoverId: '',
+      };
+    },
+  };
+</script>
+
+<style></style>
--- components/story/story-list.vue
+++ components/story/story-list.vue
@@ -23,40 +23,7 @@
         </div>
       </div>
       <!-- 顯示限時動態的卡片 -->
-      <template v-for="n in 5">
-        <div
-          :id="getId(n)"
-          :key="getId(n)"
-          class="min-w-[120px] flex-1 cursor-pointer px-[4px]"
-          @mouseover="hoverId = getId(n)"
-          @mouseleave="hoverId = ''"
-        >
-          <div class="relative overflow-hidden">
-            <div
-              :class="{ hidden: hoverId === getId(n) }"
-              class="absolute top-0 left-0 h-full w-full bg-black/20"
-            ></div>
-            <!-- 限動帳號頭像 -->
-            <div
-              class="absolute top-4 left-4 z-10 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-fb-card ring-4 ring-fb"
-            >
-              <p class="text-sm text-white">布</p>
-            </div>
-            <!-- 漸層的遮罩 -->
-            <div
-              class="absolute top-0 left-0 z-20 h-full w-full bg-gradient-to-b from-black/30 to-transparent"
-            ></div>
-            <img
-              :class="{ 'scale-105': hoverId === getId(n) }"
-              class="h-full w-full duration-500 hover:scale-105"
-              src="https://bruce-fe-fb.web.app/image/story.png"
-              alt=""
-            />
-            <!-- 限動帳號名稱 -->
-            <div class="absolute bottom-2 left-2 z-10 text-white">布魯斯</div>
-          </div>
-        </div>
-      </template>
+      <story-item v-for="n in 5" :id="getId(n)" :key="getId(n)"></story-item>
     </div>
     <button
       class="popover-btn absolute -right-3 top-[calc(50%-22.5px)] z-10 h-[45px] w-[45px]"
@@ -67,13 +34,10 @@
 </template>
 
 <script>
+  import StoryItem from './story-item.vue';
   export default {
     name: 'StoryList',
-    data() {
-      return {
-        hoverId: '',
-      };
-    },
+    components: { StoryItem },
     computed: {
       getId() {
         return (n) => `story-item-${n}`;
