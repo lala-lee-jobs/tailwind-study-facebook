c6847a813b1c686453201f4a331a70e6743fe548
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-03-04 23:55:19 +0800

    feat: 實戰仿Facebook UI，貼文樣式1 動態牆貼文

--- components/posts/posts-wall.vue
+++ components/posts/posts-wall.vue
@@ -2,15 +2,17 @@
   <div>
     <create-post class="card"></create-post>
     <live-chat class="card flex pr-0"></live-chat>
+    <posts-wrapper class="card px-0 pb-0"></posts-wrapper>
   </div>
 </template>
 
 <script>
   import CreatePost from './create-post';
   import LiveChat from './live-chat';
+  import PostsWrapper from './posts-wrapper';
   export default {
     name: 'PostsWall',
-    components: { CreatePost, LiveChat },
+    components: { CreatePost, LiveChat, PostsWrapper },
   };
 </script>
 
---
+++ components/posts/posts-wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <post-header />
+    <post-content />
+  </div>
+</template>
+
+<script>
+  import PostContent from './post-content.vue';
+  import PostHeader from './post-header.vue';
+  export default {
+    name: 'PostsWrapper',
+    components: { PostHeader, PostContent },
+  };
+</script>
+
+<style></style>
